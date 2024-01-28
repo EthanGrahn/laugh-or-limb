026f1ebc86b795f472ca07f506a670c7f7e7ca15
Radicalbot <Radicalbot@icloud.com>
2024-01-28 11:40:07 -0600

    Cannon Works
    Needs a final pos as an empty game object

--- Laugh Or Limb/Assets/Scripts/CatapultArrow.cs
+++ Laugh Or Limb/Assets/Scripts/CatapultArrow.cs
@@ -2,27 +2,24 @@ using UnityEngine;
 
 public class CatapultArrow : MonoBehaviour
 {
+    public GameObject arrow, cannon, finalCannonPos;
+    bool fired = false;
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
-            gameObject.SetActive(false);
+            arrow.SetActive(false);
+            fired = true;
+            cannon.transform.SetParent(finalCannonPos.transform, true);
             return;
         }
 
-        /*
-        if (Input.GetMouseButtonDown(0))
+        if (!fired)
         {
-            if (throwLengh < 50f)
-            {
-                throwLengh += Time.deltaTime * 5f;
-                throwBar.transform.localScale = new Vector3(0f, throwLengh, 0f);
-            }
+            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = Input.mousePosition - pos;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
-        */
-        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 dir = Input.mousePosition - pos;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
