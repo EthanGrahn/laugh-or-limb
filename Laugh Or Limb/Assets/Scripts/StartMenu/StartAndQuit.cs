832657e3abd4b99d2e162f8bfe62f7f53226d7f8
Radicalbot <Radicalbot@icloud.com>
2024-01-27 22:43:17 -0600

    Credits Button

--- Laugh Or Limb/Assets/Scripts/StartMenu/StartAndQuit.cs
+++ Laugh Or Limb/Assets/Scripts/StartMenu/StartAndQuit.cs
@@ -5,19 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class StartAndQuit : MonoBehaviour
 {
-    public string SceneName;
+    public string startGame;
     public string credits;
+    public string mainMenu;
 
     public void StartGame()
     {
         Debug.Log("StartingGame");
-        SceneManager.LoadScene(SceneName);
+        SceneManager.LoadScene(startGame);
     }
 
     public void Credits()
     {
         Debug.Log("Credits");
         SceneManager.LoadScene(credits);
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("MainMenu");
+        SceneManager.LoadScene(mainMenu);
     }
 
     public void EndGame()
